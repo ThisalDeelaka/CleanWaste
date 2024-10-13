a73d3badef813510f7bf8ef146100f79f6b29d23
ThisalDeelaka <thisaldeelakatdr@gmail.com>
2024-10-13 16:58:28 +0530

    navbar enhanced

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-[#175E5E] text-white">
+    <nav className="bg-[#0c343d] text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Logo */}
