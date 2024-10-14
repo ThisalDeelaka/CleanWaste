d59da3581383a2137eaaafab2842a6930267b1d6
ThisalDeelaka <thisaldeelakatdr@gmail.com>
2024-10-14 22:42:12 +0530

    make pickupRequestPage responsive

--- frontend/src/pages/driver/driverPickupRequest.jsx
+++ frontend/src/pages/driver/driverPickupRequest.jsx
@@ -73,7 +73,7 @@ const PickupRequests = () => {
 
       {/* Main Content */}
       <div className="flex-grow bg-gray-100 py-8 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-extrabold text-center text-[#175E5E] mb-8">
+        <h1 className="text-2xl sm:text-3xl font-extrabold text-center text-[#175E5E] mb-8">
           Waste Requests for {street}
         </h1>
         <div className="w-full max-w-4xl mx-auto">
@@ -82,10 +82,10 @@ const PickupRequests = () => {
               {wasteRequests.map((request) => (
                 <li
                   key={request._id}
-                  className="bg-white shadow-md rounded-lg p-6 border-l-4 border-blue-400"
+                  className="bg-white shadow-md rounded-lg p-4 md:p-6 border-l-4 border-blue-400"
                 >
-                  <div className="flex justify-between items-center">
-                    <div className="w-full max-w-[70%]">
+                  <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
+                    <div className="w-full md:w-[65%] mb-4 md:mb-0">
                       <p className="text-lg font-semibold text-gray-800 truncate">
                         Waste Type: {request.wasteType.join(", ")}
                       </p>
@@ -107,10 +107,10 @@ const PickupRequests = () => {
                       </p>
                     </div>
 
-                    <div className="flex flex-shrink-0 space-x-2">
+                    <div className="flex space-x-2 w-full md:w-auto justify-end">
                       {/* View Location Button */}
                       <button
-                        className="px-4 py-2 bg-yellow-500 text-white font-semibold rounded-md hover:bg-yellow-600 transition duration-200"
+                        className="w-full md:w-auto px-4 py-2 bg-yellow-500 text-white font-semibold rounded-md hover:bg-yellow-600 transition duration-200"
                         onClick={() => toggleMap(request._id)}
                       >
                         {expandedRequestId === request._id
@@ -121,7 +121,7 @@ const PickupRequests = () => {
                       {/* Picked Up Button */}
                       {request.status !== "picked-up" && (
                         <button
-                          className="px-4 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-700 transition duration-200"
+                          className="w-full md:w-auto px-4 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-700 transition duration-200"
                           onClick={() => handlePickedUpClick(request._id)}
                         >
                           Picked Up
